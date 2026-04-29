7496691f2fed8ca827680a3d4010066c25730946
UĞUR AKŞAHİN <uguraksahinn@hotmail.com>
2026-04-29 11:55:51 +0300

    Update User.java
    Signed-off-by: UĞUR AKŞAHİN <uguraksahinn@hotmail.com>

--- src/User.java
+++ src/User.java
@@ -1,63 +1,21 @@
 package Object_Oriented_Programming_E_Commerce_Application;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Product {
+public class User {
     private String name;
-    private double price;
+    private String surname;
+    private String address;
 
-    // Constructor for creating a specific product
-    public Product(String name, double price) {
+    public User(String name, String surname, String address) {
         this.name = name;
-        this.price = price;
+        this.surname = surname;
+        this.address = address;
     }
-    // Getter methods
-    public String getName() {
-        return name;
-    }
-    public double getPrice() {
-        return price;
-    }
-}
-class Basket {
-    // List to hold products in the basket
-    private List<Product> products;
-    // Constructor to initialize the basket with an empty product list
-    public Basket() {
-        products = new ArrayList<>();
-    }
-    // Method to add a product to the basket
-    public void addProductToBasket(Product product) {
-        products.add(product);
-        System.out.println(product.getName() + " added to the basket");
-    }
-    // Method to display all products in the basket
-    public void displayBasket() {
-        if (products.isEmpty()) {
-            System.out.println("The basket is empty.");
-        }
-        else {
-            System.out.println("Products in your basket:");
-            for (Product product : products) {
-                System.out.println(product.getName() + ": $" +
-                        product.getPrice());
 
-            }
-        }
+    public String getFullName() {
+        return name + " " + surname;
     }
-    public static void main(String[] args) {
-// Create some products
-        Product toothpaste = new Product("Toothpaste", 2.99);
-        Product toothbrush = new Product("Toothbrush", 1.99);
-        Product shampoo = new Product("Shampoo", 4.99);
-// Create a basket to hold products
-        Basket basket = new Basket();
-// Add products to the basket
-        basket.addProductToBasket(toothpaste);
-        basket.addProductToBasket(toothbrush);
-        basket.addProductToBasket(shampoo);
-// Display all products in the basket
-        basket.displayBasket();
+
+    public String getAddress() {
+        return address;
     }
 }
